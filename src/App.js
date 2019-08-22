116fe63329e161e98a705f9bc3f23415c8ac36ff
Gheric Santiago <gheric.santiago@directwithhotels.com>
2019-08-22 16:15:28 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer  = electron.ipcRenderer;
+
 function App() {
   return (
     <div className="App">
